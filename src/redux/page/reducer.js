cb9c5367ce0069fad6ceaca82065c1ccea5b2403
CVazquezOpalka <cristianvazquez80@gmail.com>
2023-08-28 19:28:34 -0300

    conectando los componentes al store

---
+++ src/redux/page/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  previousPage: "",
+  nextPage: "",
+  actualPage: "",
+};
+
+export const PaginationReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
---
+++ src/redux/register&login/reducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+  userName: "",
+  userToken: "",
+  userEmail: "",
+};
+
+export const LoginAndRegisterReducer = (
+  state = initialState,
+  { type, payload }
+) => {
+  switch (type) {
+    default:
+      return state;
+  }
+};
